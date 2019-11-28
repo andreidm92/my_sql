51b77ea84c322fae326d912c232edf9ee4b06ce1
andreidm92 <admi70@rambler.ru>
2019-11-29 01:26:00 +0300

    Домашняя работа к видео лекции

---
+++ Lesson6.sql
@@ -0,0 +1,25 @@
+
+/* Операторы, фильтрация, сортировка и ограничение */
+
+/* Задача 1
+1. Составьте список пользователей users, которые осуществили хотя бы один заказ orders в интернет магазине.
+*/
+Select 
+	user_id,
+	count(*),
+	(select name from users where id = user_id) as name
+from orders group by user_id;
+
+
+
+/* Задача 2
+2. Выведите список товаров products и разделов catalogs, который соответствует товару.
+*/
+
+select 
+	name,
+	description,
+	catalog_id,
+	(select name from catalogs where id = catalog_id) as catalog_name
+from products; 
+
